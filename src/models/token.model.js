2cf69cc7e7ef68a8c3f6d794546af29603562dde
SonuPriyanshuok146 <priyanshuok146@yahoo.com>
2026-03-10 11:04:56 +0530

    token are provided by admin to guard

---
+++ src/models/token.model.js
@@ -0,0 +1,35 @@
+import mongoose, { Schema } from "mongoose";
+
+const tokenSchema = new Schema(
+  {
+    tokenNumber: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+
+    isAvailable: {
+      type: Boolean,
+      default: true,
+    },
+
+    assignedOrder: {
+      type: Schema.Types.ObjectId,
+      ref: "Order",
+    },
+
+    assignedByGuard: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+
+    assignedAt: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const Token = mongoose.model("Token", tokenSchema);
